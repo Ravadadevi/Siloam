49debf6e5a031a4a03337bb582399532bdbc7dd8
Ravada Devi <deviravadadevi@gmail.com>
2025-06-03 03:39:21 -0700

    Update Locations.jsx

--- src/Locations.jsx
+++ src/Locations.jsx
@@ -83,12 +83,12 @@ function Locations(){
 
                 <div className="pages">
                     <h4>Pages</h4>
-                    <li><a href="/home">Home</a></li>
-            <li><a href="/about">About</a></li>
-            <li><a href="/team">Team</a></li>
-            <li><a href="/location">Location</a></li>
-            <li><a href="/ourtrust"> Programs</a></li>
-            <li><a href="/events">Events</a></li>
+                     <li><NavLink to="/home">Home</NavLink></li>
+          <li><NavLink to="/about">About</NavLink></li>
+          <li><NavLink to="/team">Team</NavLink></li>
+          <li><NavLink to="/location">Location</NavLink></li>
+          <li><NavLink to="/ourtrust"> Programs</NavLink></li>
+          <li><NavLink to="/events">Events</NavLink></li> 
                 </div>
 
                 <div className="icons">
