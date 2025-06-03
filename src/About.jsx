4416f47f27d9121c5c329d578fc67bda073e0dc0
Ravada Devi <deviravadadevi@gmail.com>
2025-06-03 03:11:12 -0700

    Update About.jsx

--- src/About.jsx
+++ src/About.jsx
@@ -219,9 +219,9 @@ For the sin of this one man, Adam, caused death to rule over many. But even grea
           
         </div>
         <hr />
-                      <div className="copy">
-                <p>Charity No. 1117019 | Registered in England & Wales No. 5996380.</p>
-                <p>© 2025 Renewal Church. All rights reserved.</p>
+ <div className="copy">
+                <p>Looma It Solutions PVT.Ltd</p>
+                <p>© 2025 siloam Church. All rights reserved.</p>
         </div>
 
     
