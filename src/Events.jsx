b6c4f48f7c70a0f6416c874e4cd82cb03597a8e9
Ravada Devi <deviravadadevi@gmail.com>
2025-06-04 03:35:49 -0700

    Update Events.jsx

--- src/Events.jsx
+++ src/Events.jsx
@@ -241,7 +241,7 @@ const Events = () => {
             <br /> 
             <div className="foot">
               <div>
-              <img src="/jesus.webp" alt="" />
+                          <img src="/jesuschrist.jpeg" alt="" />
               </div>
                 <div className="reach">
                 <h4>Reach us</h4>
@@ -314,7 +314,7 @@ const Events = () => {
         </div>
         <hr />
               <div className="copy">
-                <p>Looma It Solutions PVT.Ltd</p>
+                <p>Lomaa IT Solutions</p>
                 <p>© 2025 siloam Church. All rights reserved.</p>
         </div>
     
